455e42160f969cc3062808c89ea26173630ff724
Agustin Maza <Juan.MazaEstrada@alu.frt.utn.edu.ar>
2026-05-01 17:51:53 -0300

    Added class Problema2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Numerics;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal total = quantity <= 0 ? 0 : quantity * unitPrice;
+
+            var product = new { productCode, productDescription, quantity, total };
+
+            return $"{product.productCode}-{product.productDescription}-{product.total:C2}";
+        }
+    }
+}
