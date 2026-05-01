e6fa1d9166849baf461d12017122d3aca7828493
Agustin Maza <Juan.MazaEstrada@alu.frt.utn.edu.ar>
2026-05-01 17:14:21 -0300

    Added partial class ProductHelper

---
+++ Dsw2026Ej8/ProductHelperAux.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public partial class ProductHelper
+    {
+        private string FormatoEtiqueta(long code, string description, decimal price)
+        {
+            return $"{code}, {description} - {price:C2}";
+        }
+    }
+}
