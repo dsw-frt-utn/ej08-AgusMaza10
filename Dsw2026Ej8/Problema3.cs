12bec0417a1e0e7f5e6ff6895c2c1ee4c59b488f
Agustin Maza <Juan.MazaEstrada@alu.frt.utn.edu.ar>
2026-05-01 18:24:18 -0300

    Added class Problema3 and class Product

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int copiaValue = originalValue;
+            copiaValue++;
+            Product product2 = product;
+            product2.SetDescripcionProducto("Nuevo");
+            return $"{originalValue}-{copiaValue}-{product2.GetDescripcionProducto()}";
+        }
+
+    }
+}
---
+++ Dsw2026Ej8/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Product
+    {
+        private string _descripcionProducto = "";
+
+        public void SetDescripcionProducto(string descripcionProducto)
+        {
+            _descripcionProducto = descripcionProducto;
+        }
+
+        public string GetDescripcionProducto()
+        { 
+            return _descripcionProducto; 
+        }
+    }
+}
