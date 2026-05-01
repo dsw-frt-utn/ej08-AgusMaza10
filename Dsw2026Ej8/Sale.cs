77e9cc26b398b458ec77588bb5da01ccd6fbdb89
Agustin Maza <Juan.MazaEstrada@alu.frt.utn.edu.ar>
2026-05-01 19:19:05 -0300

    Added class Problema5, virtual class Sale and overrides

---
+++ Dsw2026Ej8/Sale.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Sale
+    {
+        private decimal _price;
+        private int _quantity;
+
+        public void SetPrice(decimal price)
+        {
+            _price = price;
+        }
+        public void SetQuantity(int quantity)
+        {
+            _quantity = quantity;
+        }
+
+        public decimal GetPrice()
+        {
+            return _price;
+        }
+
+        public int GetQuantity()
+        {
+            return _quantity;
+        }
+
+        public virtual decimal CalculateTotal()
+        {
+            return _price * _quantity;
+        }
+    }
+}
