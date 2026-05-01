18551ff5aac7ab11244341ddf34c1fe30a829372
Agustin Maza <Juan.MazaEstrada@alu.frt.utn.edu.ar>
2026-05-01 19:39:03 -0300

    Added class Problema6 and class Extensions

---
+++ Dsw2026Ej8/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public static class Extensions
+    {
+        public static string ToProductCode(this string code)
+        {
+            if (code is null)
+                return "SIN-CODIGO";
+            else
+            {
+                string result = code;
+                result = result.Trim();
+                result = result.ToUpper();
+                result = result.Replace(" ","-");
+                return result;
+            }
+        }
+    }
+}
